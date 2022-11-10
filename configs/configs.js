01a760b554b46b4db0eb92c2744ec4e99a363c6b
User <alinatyndyk777@gmail.con>
2022-11-10 15:14:53 +0200

    initial commit

--- configs/configs.js
+++ configs/configs.js
@@ -2,8 +2,8 @@ module.exports = {
     PORT: process.env.PORT || 5000,
     MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/test',
 
-    NO_REPLY_EMAIL: process.env.NO_REPLY_EMAIL || 'haisicraisi@gmail.com',
-    NO_REPLY_PASSWORD: process.env.NO_REPLY_PASSWORD || 'qvjaspcpnglrmggf',
+    NO_REPLY_EMAIL: process.env.NO_REPLY_MAIL || 'haisicraisi@gmail.com',
+    NO_REPLY_PASSWORD: process.env.NO_REPLY_PASS || 'qvjaspcpnglrmggf',
 
     NEW_POSITION_URL: process.env.NEW_POSITION_URL || 'http://localhost:5000/positions/'
 }
